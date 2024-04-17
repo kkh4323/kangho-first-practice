521315d3e1f1ced575f434af319381495cd6c8c3
Kangho <kkh4323@naver.com>
2024-04-18 00:30:44 +0900

    Connect Database #2 & Entity 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { BlogModule } from './blog/blog.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi'; // JavaScript 객체 유효성 검사 라이브러리
 
 @Module({
-  imports: [BlogModule, DatabaseModule, ConfigModule.forRoot()],
+  imports: [
+    BlogModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        SERVER_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'node:process';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'floppy.db.elephantsql.com',
-      port: 5432,
-      username: 'ahcpkthv',
-      password: 'iikosKhRiHD73xOAdaCaINGj-SozMZxJ',
-      database: 'ahcpkthv',
-      entities: [],
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        // root의 모든 하위 디렉토리의 하위 디렉토리에 있는 모든 .entity.js 나 .entity.ts를 불러옴
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
   ],
 })
